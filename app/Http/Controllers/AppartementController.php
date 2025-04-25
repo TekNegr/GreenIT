ac89f9972844fe9231a5fe80714d97a86e2772b2
Hector <djoukwekombou9@gmail.com>
2025-04-25 21:12:46 +0200

    addLogementToDB

--- app/Http/Controllers/AppartementController.php
+++ app/Http/Controllers/AppartementController.php
@@ -28,4 +28,58 @@ class AppartementController extends Controller
 
         return response()->json(['message' => 'ImportDpeData job dispatched', 'bbox' => $bboxArray]);
     }
+
+    function addLogementToDB($json)
+    {
+        // Extraire l'adresse depuis le JSON
+        $adresse = $json['address'];
+
+        // Vérifier si le bâtiment existe déjà
+        if (!Batiment::where('adresse', $adresse)->exists()) {
+            // Créer un nouveau bâtiment si l'adresse n'existe pas
+            $batiment = Batiment::create([
+                'id',
+                'adresse' => $adresse,
+                'latitude' => $json['latitude'],
+                'longitude' => $json['longitude'],
+                'type' => $json['building_type'],
+                'avg_dpe_grade',
+                'avg_ges_grade',
+                'avg_energy_consumption',
+                'avg_carbon_emission',
+                'apartments_count',
+            ]);
+        } else {
+            // Récupérer le bâtiment existant
+            $batiment = Batiment::where('adresse', $adresse)->first();
+        }
+
+        // Vérifier si l'appartement existe déjà avec le dpe_number
+        if (Appartement::where('dpe_number', $json['dpe_number'])->exists()) {
+            return 0; // L'appartement existe déjà, ne rien faire
+        }
+
+        // Créer un nouvel appartement
+        Appartement::create([
+            
+            'dpe_number' => $json['dpe_number'],
+            'energy_class' => $json['energy_class'],
+            'construction_year' => $json['construction_year'],
+            'surface_area' => $json['surface_area'],
+            'energy_consumption' => $json['energy_consumption'],
+            'ges_estimation' => $json['ges_estimation'],
+            'longitude'  => $json['longitude'],
+            'latitude' => $json['latitude'],
+            'address' => $json['geo_adresse'],
+            'year_built' => $json['annee_construction'],
+            'ges_grade' => $json['classe_estimationion_ges'],
+            'carbon_emission' => $json['carbon_emission'],
+            'batiment_id' => $json['batiment_id'],
+            'building_id' => $batiment->id,
+            
+            
+        ]);
+
+        return 1; // Succès
+    }
 }
