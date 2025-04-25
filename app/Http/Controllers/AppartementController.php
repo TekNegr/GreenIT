37ad684fa5215ff0a851937b15dc146a6bda677c
Henin <94859764+TekNegr@users.noreply.github.com>
2025-04-25 21:16:54 +0200

    Ajustement dpd

--- app/Http/Controllers/AppartementController.php
+++ app/Http/Controllers/AppartementController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Jobs\ImportDpeData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Models\Batiment;
+use App\Models\Appartement;
 
 class AppartementController extends Controller
 {
