81fd7e90e618c4bb9fa5a1a5a275ecf867269021
Henin <94859764+TekNegr@users.noreply.github.com>
2025-04-26 17:38:16 +0200

    fait pas attention à moi2

--- app/Http/Controllers/DPEController.php
+++ app/Http/Controllers/DPEController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class DPEController extends Controller
 {
-    //
+    public function moyenne(Request $request)
+    {
+        $donnees = $request->input('batiments', []);
+        $resultats = DpeCalculator::moyenne($donnees);
+
+        return response()->json($resultats);
+    }
 }
