5b7221d05f018bfec18aa9cbe430ecd3bbc6c6aa
titims25-mg <timothe.rajanelson@gmail.com>
2025-04-30 14:46:23 +0200

    vue de filtre appartement/building

---
+++ resources/js/components/MapFilter.vue
@@ -0,0 +1,5 @@
+<select v-model="selectedType" @change="updateFilter">
+  <option value="">Tous</option>
+  <option value="building">Bâtiment</option>
+  <option value="appartement">Appartement</option>
+</select>
